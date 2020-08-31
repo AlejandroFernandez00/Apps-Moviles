568a625c20ef7c58fa6f4846fc70dfdbf1864e01
Alejandro <foxterosa@gmail.com>
2020-08-31 10:00:23 -0300

    Ejercicios 1 al 3

---
+++ Ejercicios Resueltos/3ejercicio 3.js
@@ -0,0 +1,11 @@
+var Numero = 8;
+
+function esPar(Numero)
+{
+    if (Numero %2 ===0){
+        return "Es divisible"
+    }else{
+        return "No es divisible"
+    }
+}
+console.log(esPar(Numero));
